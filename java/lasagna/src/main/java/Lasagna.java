ffbd60bf4acc8db4b9656d7f221ca202e2bb95c0
Paweł J <jpawz@outlook.com>
2023-03-06 20:40:52 +0100

    Refactor

--- java/lasagna/src/main/java/Lasagna.java
+++ java/lasagna/src/main/java/Lasagna.java
@@ -1,17 +1,43 @@
 public class Lasagna {
+	private static final int COOKING_TIME_IN_MINUTES = 40;
+	private static final int MINUTES_PREPARATION_PER_LAYER = 2;
 
+	/**
+	 * Gives the cooking time of the lasagna.
+	 * 
+	 * @return time in minutes to cook the lasagna.
+	 */
 	public int expectedMinutesInOven() {
-		return 40;
+		return COOKING_TIME_IN_MINUTES;
 	}
 
+	/**
+	 * Calculates the remaining time to cook the lasagna.
+	 * 
+	 * @param actualMinutesInOven how many minutes is the lasagna currently cooking.
+	 * @return remaining time in minutes to cook the lasagna.
+	 */
 	public int remainingMinutesInOven(int actualMinutesInOven) {
 		return expectedMinutesInOven() - actualMinutesInOven;
 	}
 
+	/**
+	 * Calculates preparation time in minutes of lasagna based on number of layers.
+	 * 
+	 * @param numberOfLayers
+	 * @return preparation time in minutes of the lasagna.
+	 */
 	public int preparationTimeInMinutes(int numberOfLayers) {
-		return numberOfLayers * 2;
+		return numberOfLayers * MINUTES_PREPARATION_PER_LAYER;
 	}
 
+	/**
+	 * Calculates total preparation time of lasagna.
+	 * 
+	 * @param numberOfLayers
+	 * @param actualMinutesInOven
+	 * @return time in minutes.
+	 */
 	public int totalTimeInMinutes(int numberOfLayers, int actualMinutesInOven) {
 		return preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
 	}
