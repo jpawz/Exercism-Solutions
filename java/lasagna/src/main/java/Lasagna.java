3764b2669bd87c41680006b0e2e70c13da93d42c
Paweł J <jpawz@outlook.com>
2023-03-06 20:16:30 +0100

    Add remainingMinutesInOven method

--- java/lasagna/src/main/java/Lasagna.java
+++ java/lasagna/src/main/java/Lasagna.java
@@ -1,10 +1,12 @@
 public class Lasagna {
-	// TODO: define the 'expectedMinutesInOven()' method
+
 	public int expectedMinutesInOven() {
 		return 40;
 	}
 
-	// TODO: define the 'remainingMinutesInOven()' method
+	public int remainingMinutesInOven(int actualMinutesInOven) {
+		return expectedMinutesInOven() - actualMinutesInOven;
+	}
 
 	// TODO: define the 'preparationTimeInMinutes()' method
 
