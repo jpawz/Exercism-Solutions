e4012739fa6494cc99f7d68ed10e8df129d08d5d
Paweł J <jpawz@outlook.com>
2023-03-07 20:30:52 +0100

    Implement canFreePrisoner method

--- java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
+++ java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -13,7 +13,12 @@ class AnnalynsInfiltration {
 
 	public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake,
 			boolean petDogIsPresent) {
-		throw new UnsupportedOperationException(
-				"Please implement the (static) AnnalynsInfiltration.canFreePrisoner() method");
+		if (petDogIsPresent && !archerIsAwake) {
+			return true;
+		}
+		if (!petDogIsPresent && prisonerIsAwake && !knightIsAwake && !archerIsAwake) {
+			return true;
+		}
+		return false;
 	}
 }
