67eb01b3f84e8a4231eeb0b5bc9c91f65a3d3932
Paweł J <jpawz@outlook.com>
2023-03-08 18:41:58 +0100

    Complete exercise log-levels

--- java/log-levels/src/main/java/LogLevels.java
+++ java/log-levels/src/main/java/LogLevels.java
@@ -1,14 +1,14 @@
 public class LogLevels {
-    
-    public static String message(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.message() method");
-    }
 
-    public static String logLevel(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.logLevel() method");
-    }
+	public static String message(String logLine) {
+		return logLine.substring(logLine.indexOf(":") + 1).trim();
+	}
 
-    public static String reformat(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.reformat() method");
-    }
+	public static String logLevel(String logLine) {
+		return logLine.substring(logLine.indexOf("[") + 1, logLine.indexOf("]")).toLowerCase();
+	}
+
+	public static String reformat(String logLine) {
+		return message(logLine) + " (" + logLevel(logLine) + ")";
+	}
 }
