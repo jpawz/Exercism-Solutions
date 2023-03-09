aceafdc3515b3ca405caa60f79696af3170832c7
Paweł J <jpawz@outlook.com>
2023-03-09 21:22:52 +0100

    Change kebab case to camel case

--- java/squeaky-clean/src/main/java/SqueakyClean.java
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,6 +1,11 @@
+import java.util.regex.Pattern;
+
 class SqueakyClean {
 	static String clean(String identifier) {
-
-		return identifier.replace(" ", "_").replaceAll("\\W", "CTRL");
+		String withoutSpaces = identifier.replace(" ", "_");
+		String withoutControlCharacters = withoutSpaces.replaceAll("\\p{Cc}", "CTRL");
+		String camelCaseString = Pattern.compile("-(.)").matcher(withoutControlCharacters)
+				.replaceAll(mr -> mr.group(1).toUpperCase());
+		return camelCaseString;
 	}
 }
