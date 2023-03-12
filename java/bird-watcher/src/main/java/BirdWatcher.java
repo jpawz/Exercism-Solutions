ea8004df534f836b8198d8ffbb688e661fc71d10
Paweł J <jpawz@outlook.com>
2023-03-12 17:51:55 +0100

    Implement getLastWeek method

--- java/bird-watcher/src/main/java/BirdWatcher.java
+++ java/bird-watcher/src/main/java/BirdWatcher.java
@@ -1,32 +1,33 @@
+import java.util.Arrays;
 
 class BirdWatcher {
-    private final int[] birdsPerDay;
+	private final int[] birdsPerDay;
 
-    public BirdWatcher(int[] birdsPerDay) {
-        this.birdsPerDay = birdsPerDay.clone();
-    }
+	public BirdWatcher(int[] birdsPerDay) {
+		this.birdsPerDay = birdsPerDay.clone();
+	}
 
-    public int[] getLastWeek() {
-        throw new UnsupportedOperationException("Please implement the BirdCount.getLastWeek() method");
-    }
+	public int[] getLastWeek() {
+		return Arrays.copyOfRange(birdsPerDay, 0, 7);
+	}
 
-    public int getToday() {
-        throw new UnsupportedOperationException("Please implement the BirdCount.getToday() method");
-    }
+	public int getToday() {
+		throw new UnsupportedOperationException("Please implement the BirdCount.getToday() method");
+	}
 
-    public void incrementTodaysCount() {
-        throw new UnsupportedOperationException("Please implement the BirdCount.incrementTodaysCount() method");
-    }
+	public void incrementTodaysCount() {
+		throw new UnsupportedOperationException("Please implement the BirdCount.incrementTodaysCount() method");
+	}
 
-    public boolean hasDayWithoutBirds() {
-        throw new UnsupportedOperationException("Please implement the BirdCount.hasDayWithoutBirds() method");
-    }
+	public boolean hasDayWithoutBirds() {
+		throw new UnsupportedOperationException("Please implement the BirdCount.hasDayWithoutBirds() method");
+	}
 
-    public int getCountForFirstDays(int numberOfDays) {
-        throw new UnsupportedOperationException("Please implement the BirdCount.getCountForFirstDays() method");
-    }
+	public int getCountForFirstDays(int numberOfDays) {
+		throw new UnsupportedOperationException("Please implement the BirdCount.getCountForFirstDays() method");
+	}
 
-    public int getBusyDays() {
-        throw new UnsupportedOperationException("Please implement the BirdCount.getBusyDays() method");
-    }
+	public int getBusyDays() {
+		throw new UnsupportedOperationException("Please implement the BirdCount.getBusyDays() method");
+	}
 }
