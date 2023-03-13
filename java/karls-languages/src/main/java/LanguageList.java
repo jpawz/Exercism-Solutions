68671a03cc606e415f40177df8bc421afd324b65
Paweł J <jpawz@outlook.com>
2023-03-13 19:32:17 +0100

    Complete karls-language exercise

--- java/karls-languages/src/main/java/LanguageList.java
+++ java/karls-languages/src/main/java/LanguageList.java
@@ -1,34 +1,40 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class LanguageList {
-    private final List<String> languages = new ArrayList<>();
+	private final List<String> languages = new ArrayList<>();
 
-    public boolean isEmpty() {
-        throw new UnsupportedOperationException("Please implement the isEmpty() method");
-    }
+	public boolean isEmpty() {
+		return languages.isEmpty();
+	}
 
-    public void addLanguage(String language) {
-        throw new UnsupportedOperationException("Please implement the addLanguage() method");
-    }
+	public void addLanguage(String language) {
+		languages.add(language);
+	}
 
-    public void removeLanguage(String language) {
-        throw new UnsupportedOperationException("Please implement the removeLanguage() method");
-    }
+	public void removeLanguage(String language) {
+		languages.remove(language);
+	}
 
-    public String firstLanguage() {
-        throw new UnsupportedOperationException("Please implement the firstLanguage() method");
-    }
+	public String firstLanguage() {
+		if (languages.size() > 0) {
+			return languages.get(0);
+		}
+		return "";
+	}
 
-    public int count() {
-        throw new UnsupportedOperationException("Please implement the count() method");
-    }
+	public int count() {
+		return languages.size();
+	}
 
-    public boolean containsLanguage(String language) {
-        throw new UnsupportedOperationException("Please implement the containsLanguage() method");
-    }
+	public boolean containsLanguage(String language) {
+		return languages.contains(language);
+	}
 
-    public boolean isExciting() {
-        throw new UnsupportedOperationException("Please implement the isExciting() method");
-    }
+	public boolean isExciting() {
+		Predicate<String> isJavaOrKotlin = language -> language == "Java" || language == "Kotlin";
+		return languages.stream()
+				.anyMatch(isJavaOrKotlin);
+	}
 }
