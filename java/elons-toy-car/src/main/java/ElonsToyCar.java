a8e1661636a34def009a2417ad63fa6ad4671bbd
Paweł J <jpawz@outlook.com>
2023-03-15 20:17:32 +0100

    Fix drive when battery is empty

--- java/elons-toy-car/src/main/java/ElonsToyCar.java
+++ java/elons-toy-car/src/main/java/ElonsToyCar.java
@@ -11,10 +11,17 @@ public class ElonsToyCar {
 	}
 
 	public String batteryDisplay() {
-		return "Battery at " + batteryCharge + "%";
+		if (batteryCharge == 0) {
+			return "Battery empty";
+		} else {
+			return "Battery at " + batteryCharge + "%";
+		}
 	}
 
 	public void drive() {
+		if (batteryCharge == 0)
+			return;
+
 		distance += 20;
 		batteryCharge -= 1;
 	}
