87009edf9a987995493b49ee08ef926f2cabfc90
Paweł J <jpawz@outlook.com>
2023-03-20 19:25:36 +0100

    Complete exercise

--- java/salary-calculator/src/main/java/SalaryCalculator.java
+++ java/salary-calculator/src/main/java/SalaryCalculator.java
@@ -1,17 +1,20 @@
 public class SalaryCalculator {
-    public double multiplierPerDaysSkipped(int daysSkipped) {
-        throw new UnsupportedOperationException("Please implement the SalaryCalculator.multiplierPerDaysSkipped() method");
-    }
+	private static final double BASE_SALARY = 1000.0;
 
-    public int multiplierPerProductsSold(int productsSold) {
-        throw new UnsupportedOperationException("Please implement the SalaryCalculator.multiplierPerProductsSold() method");
-    }
+	public double multiplierPerDaysSkipped(int daysSkipped) {
+		return daysSkipped > 5 ? 0.85 : 1;
+	}
 
-    public double bonusForProductSold(int productsSold) {
-        throw new UnsupportedOperationException("Please implement the SalaryCalculator.bonusForProductSold() method");
-    }
+	public int multiplierPerProductsSold(int productsSold) {
+		return productsSold > 10 ? 13 : 10;
+	}
 
-    public double finalSalary(int daysSkipped, int productsSold) {
-        throw new UnsupportedOperationException("Please implement the SalaryCalculator.finalSalary() method");
-    } 
+	public double bonusForProductSold(int productsSold) {
+		return productsSold * multiplierPerProductsSold(productsSold);
+	}
+
+	public double finalSalary(int daysSkipped, int productsSold) {
+		double salary = BASE_SALARY * multiplierPerDaysSkipped(daysSkipped) + bonusForProductSold(productsSold);
+		return salary > 2000 ? 2000 : salary;
+	}
 }
