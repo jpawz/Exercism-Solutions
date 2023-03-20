7d299e629f88f7ac3f869b99c1ae43f88b49efea
Paweł J <jpawz@outlook.com>
2023-03-20 19:29:06 +0100

    Refactor

--- java/salary-calculator/src/main/java/SalaryCalculator.java
+++ java/salary-calculator/src/main/java/SalaryCalculator.java
@@ -1,12 +1,15 @@
 public class SalaryCalculator {
+	private static final int DAYS_SKIPPED_THRESHOLD = 5;
+	private static final int PRODUCT_SOLD_THRESHOLD = 10;
 	private static final double BASE_SALARY = 1000.0;
+	private static final double SALARY_LIMIT = 2000.0;
 
 	public double multiplierPerDaysSkipped(int daysSkipped) {
-		return daysSkipped > 5 ? 0.85 : 1;
+		return daysSkipped > DAYS_SKIPPED_THRESHOLD ? 0.85 : 1;
 	}
 
 	public int multiplierPerProductsSold(int productsSold) {
-		return productsSold > 10 ? 13 : 10;
+		return productsSold > PRODUCT_SOLD_THRESHOLD ? 13 : 10;
 	}
 
 	public double bonusForProductSold(int productsSold) {
@@ -15,6 +18,6 @@ public class SalaryCalculator {
 
 	public double finalSalary(int daysSkipped, int productsSold) {
 		double salary = BASE_SALARY * multiplierPerDaysSkipped(daysSkipped) + bonusForProductSold(productsSold);
-		return salary > 2000 ? 2000 : salary;
+		return salary > SALARY_LIMIT ? SALARY_LIMIT : salary;
 	}
 }
