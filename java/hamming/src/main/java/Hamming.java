6766b336668c0a3d2166360f94f43a52ae79a0e5
Paweł J <jpawz@outlook.com>
2023-03-26 19:01:41 +0200

    Implement getHammingDisatance method

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,9 +1,28 @@
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
+
 public class Hamming {
+	private final String leftStrand;
+	private final String rightStrand;
+
 	public Hamming(String leftStrand, String rightStrand) {
-		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+		this.leftStrand = leftStrand;
+		this.rightStrand = rightStrand;
 	}
 
 	public int getHammingDistance() {
-		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+		int differences = 0;
+
+		CharacterIterator leftStrandIterator = new StringCharacterIterator(leftStrand);
+		CharacterIterator rightStrandIterator = new StringCharacterIterator(rightStrand);
+
+		while ((leftStrandIterator.current() != CharacterIterator.DONE)
+				&& (rightStrandIterator.current() != CharacterIterator.DONE)) {
+			if (leftStrandIterator.current() != rightStrandIterator.current()) {
+				differences++;
+			}
+		}
+
+		return differences;
 	}
 }
