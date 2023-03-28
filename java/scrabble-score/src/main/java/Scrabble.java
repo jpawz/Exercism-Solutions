c9e8e8aef6e03761a60053de50ca06bb19fb9b38
Paweł J <jpawz@outlook.com>
2023-03-28 20:21:57 +0200

    Complete exercise

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,11 +1,27 @@
 class Scrabble {
+	private final String word;
 
-    Scrabble(String word) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	Scrabble(String word) {
+		this.word = word;
+	}
 
-    int getScore() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	int getScore() {
+		return word.chars()
+				.map(c -> letterToValue((char) c))
+				.sum();
+	}
+
+	private static int letterToValue(char letter) {
+		return switch (Character.toUpperCase(letter)) {
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T' -> 1;
+		case 'D', 'G' -> 2;
+		case 'B', 'C', 'M', 'P' -> 3;
+		case 'F', 'H', 'V', 'W', 'Y' -> 4;
+		case 'K' -> 5;
+		case 'J', 'X' -> 8;
+		case 'Q', 'Z' -> 10;
+		default -> 0;
+		};
+	}
 
 }
