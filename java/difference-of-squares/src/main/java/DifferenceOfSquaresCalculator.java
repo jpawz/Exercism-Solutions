f4f8da3581a074de73c15f66e47c0d648e22cc87
Paweł J <jpawz@outlook.com>
2023-03-29 20:36:14 +0200

    Compute square of sum

--- java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,15 +1,18 @@
+import java.util.stream.IntStream;
+
 class DifferenceOfSquaresCalculator {
 
-    int computeSquareOfSumTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	int computeSquareOfSumTo(int input) {
+		return (int) Math.pow(IntStream.rangeClosed(1, input)
+				.sum(), 2);
+	}
 
-    int computeSumOfSquaresTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	int computeSumOfSquaresTo(int input) {
+		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+	}
 
-    int computeDifferenceOfSquares(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	int computeDifferenceOfSquares(int input) {
+		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+	}
 
 }
