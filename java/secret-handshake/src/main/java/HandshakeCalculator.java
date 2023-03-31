003758f21caa216ec90886fcde3ec4aae1366cda
Paweł J <jpawz@outlook.com>
2023-03-31 18:55:54 +0200

    Change mapper method to HashMap collection, refactor for loop

--- java/secret-handshake/src/main/java/HandshakeCalculator.java
+++ java/secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,19 +1,32 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class HandshakeCalculator {
+
+	private static final int handshakeLength = 4;
+
+	private static final Map<Integer, Signal> signalsMap = new HashMap<>(handshakeLength);
+
+	static {
+		signalsMap.put(0, Signal.WINK);
+		signalsMap.put(1, Signal.DOUBLE_BLINK);
+		signalsMap.put(2, Signal.CLOSE_YOUR_EYES);
+		signalsMap.put(3, Signal.JUMP);
+	}
 
 	List<Signal> calculateHandshake(int number) {
 		List<Signal> signals = new ArrayList<>();
 
-		for (int i = 0; i < 5; i++) {
-			if ((number % 2 == 1) && i != 4) {
-				signals.add(numberToSignal(i));
-			}
-
-			if (i == 4 && number % 2 == 1) {
-				Collections.reverse(signals);
+		for (int i = 0; i <= handshakeLength; i++) {
+			if (number % 2 == 1) {
+				if (!isLastDigit(i)) {
+					signals.add(signalsMap.get(i));
+				} else {
+					Collections.reverse(signals);
+				}
 			}
 
 			number = number / 2;
@@ -22,14 +35,8 @@ class HandshakeCalculator {
 		return signals;
 	}
 
-	private static Signal numberToSignal(int number) {
-		return switch (number) {
-		case 0 -> Signal.WINK;
-		case 1 -> Signal.DOUBLE_BLINK;
-		case 2 -> Signal.CLOSE_YOUR_EYES;
-		case 3 -> Signal.JUMP;
-		default -> throw new IllegalArgumentException("Unexpected value: " + number);
-		};
+	private boolean isLastDigit(int i) {
+		return i == handshakeLength;
 	}
 
 }
