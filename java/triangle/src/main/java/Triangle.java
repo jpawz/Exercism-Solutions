b24be733d05fb637416185da3aa932bb859ada8a
Paweł J <jpawz@outlook.com>
2023-04-05 22:00:05 +0200

    Complete exercise

--- java/triangle/src/main/java/Triangle.java
+++ java/triangle/src/main/java/Triangle.java
@@ -4,6 +4,9 @@ class Triangle {
 	private final double side3;
 
 	Triangle(double side1, double side2, double side3) throws TriangleException {
+		validateLengths(side1, side2, side3);
+		checkIneguality(side1, side2, side3);
+
 		this.side1 = side1;
 		this.side2 = side2;
 		this.side3 = side3;
@@ -14,11 +17,22 @@ class Triangle {
 	}
 
 	boolean isIsosceles() {
-		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+		return side1 == side2 || side1 == side3 || side2 == side3;
 	}
 
 	boolean isScalene() {
-		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+		return side1 != side2 && side1 != side3;
 	}
 
+	private void validateLengths(double side1, double side2, double side3) throws TriangleException {
+		if (side1 == 0 || side2 == 0 || side3 == 0) {
+			throw new TriangleException();
+		}
+	}
+
+	private void checkIneguality(double side1, double side2, double side3) throws TriangleException {
+		if (side1 + side2 < side3 || side1 + side3 < side2 || side2 + side3 < side1) {
+			throw new TriangleException();
+		}
+	}
 }
