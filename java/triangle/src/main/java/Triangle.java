e7cbeff7deb35ac59daaa7a3f824204ef1b86440
Paweł J <jpawz@outlook.com>
2023-04-05 22:17:32 +0200

    Fix isScalene

--- java/triangle/src/main/java/Triangle.java
+++ java/triangle/src/main/java/Triangle.java
@@ -5,7 +5,7 @@ class Triangle {
 
 	Triangle(double side1, double side2, double side3) throws TriangleException {
 		validateLengths(side1, side2, side3);
-		checkIneguality(side1, side2, side3);
+		checkInequality(side1, side2, side3);
 
 		this.side1 = side1;
 		this.side2 = side2;
@@ -21,7 +21,7 @@ class Triangle {
 	}
 
 	boolean isScalene() {
-		return side1 != side2 && side1 != side3;
+		return side1 != side2 && side1 != side3 && side2 != side3;
 	}
 
 	private void validateLengths(double side1, double side2, double side3) throws TriangleException {
@@ -30,7 +30,7 @@ class Triangle {
 		}
 	}
 
-	private void checkIneguality(double side1, double side2, double side3) throws TriangleException {
+	private void checkInequality(double side1, double side2, double side3) throws TriangleException {
 		if (side1 + side2 < side3 || side1 + side3 < side2 || side2 + side3 < side1) {
 			throw new TriangleException();
 		}
