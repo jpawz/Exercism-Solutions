bc3c642557978ed8b3abf61c2fa4b21c310bae66
Paweł J <jpawz@outlook.com>
2023-04-05 21:44:55 +0200

    Implement constructor

--- java/triangle/src/main/java/Triangle.java
+++ java/triangle/src/main/java/Triangle.java
@@ -1,19 +1,24 @@
 class Triangle {
+	private final double side1;
+	private final double side2;
+	private final double side3;
 
-    Triangle(double side1, double side2, double side3) throws TriangleException {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	Triangle(double side1, double side2, double side3) throws TriangleException {
+		this.side1 = side1;
+		this.side2 = side2;
+		this.side3 = side3;
+	}
 
-    boolean isEquilateral() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	boolean isEquilateral() {
+		return side1 == side2 && side1 == side3;
+	}
 
-    boolean isIsosceles() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	boolean isIsosceles() {
+		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+	}
 
-    boolean isScalene() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	boolean isScalene() {
+		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+	}
 
 }
