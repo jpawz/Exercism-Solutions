9125ff417b99189958e01daec470e037c6fc675e
Paweł J <jpawz@outlook.com>
2023-04-06 19:45:04 +0200

    Implement shiftCodePoint algorithm

--- java/rotational-cipher/src/main/java/RotationalCipher.java
+++ java/rotational-cipher/src/main/java/RotationalCipher.java
@@ -7,9 +7,19 @@ class RotationalCipher {
 
 	String rotate(String data) {
 		return data.codePoints()
-				.map(c -> c + shiftKey)
+				.map(this::shiftCodePoint)
 				.collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
 				.toString();
+	}
+
+	private int shiftCodePoint(int codePoint) {
+		if (codePoint >= 'a' && codePoint <= 'z') {
+			return (codePoint + shiftKey) <= 'z' ? codePoint + shiftKey : codePoint + shiftKey - 'z' + 'a' - 1;
+		}
+		if (codePoint >= 'A' && codePoint <= 'Z') {
+			return (codePoint + shiftKey) <= 'Z' ? codePoint + shiftKey : codePoint + shiftKey - 'Z' + 'A' - 1;
+		}
+		return codePoint;
 	}
 
 }
