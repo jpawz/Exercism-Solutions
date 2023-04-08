988a9649e792abb940fe867c0a31b9ee98fe4952
Paweł J <jpawz@outlook.com>
2023-04-08 20:13:28 +0200

    Complete exercise

--- java/saddle-points/src/main/java/Matrix.java
+++ java/saddle-points/src/main/java/Matrix.java
@@ -1,13 +1,30 @@
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class Matrix {
 
-    Matrix(List<List<Integer>> values) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+  private final int rows;
+  private final int cols;
+  private final int[] rowsMax;
+  private final int[] colsMin;
 
-    Set<MatrixCoordinate> getSaddlePoints() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+  Matrix(List<List<Integer>> values) {
+    rows = values.size();
+    cols = rows > 0 ? values.get(0).size() : 0;
+
+    rowsMax = values.stream()
+        .mapToInt(row -> row.stream().mapToInt(Integer::intValue).max().orElseThrow()).toArray();
+    colsMin = IntStream.range(0, cols)
+        .map(col -> IntStream.range(0, rows).map(i -> values.get(i).get(col)).min().orElseThrow())
+        .toArray();
+  }
+
+  Set<MatrixCoordinate> getSaddlePoints() {
+    return IntStream.range(0, rows).boxed()
+        .flatMap(row -> IntStream.range(0, cols).filter(col -> rowsMax[row] == colsMin[col])
+            .mapToObj(col -> new MatrixCoordinate(row + 1, col + 1)))
+        .collect(Collectors.toSet());
+  }
 }
