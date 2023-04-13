a4c0b1b74457d3045be560bbf7216243b03262cf
Paweł J <jpawz@outlook.com>
2023-04-13 19:56:08 +0200

    Resolve compilation errors

--- java/flatten-array/src/main/java/Flattener.java
+++ java/flatten-array/src/main/java/Flattener.java
@@ -1,10 +1,8 @@
-/*
+import java.util.List;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+class Flattener {
 
-Please remove this comment when submitting your solution.
-
-*/
+    List<Object> flatten(List<Object> list) {
+	return null;
+    }
+}
