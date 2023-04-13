87c18d264075785f2ae4a3336dcf64daca0d8570
Paweł J <jpawz@outlook.com>
2023-04-13 20:57:11 +0200

    Pass testASingleLevelOfNestingWithNoNulls

--- java/flatten-array/src/main/java/Flattener.java
+++ java/flatten-array/src/main/java/Flattener.java
@@ -1,8 +1,18 @@
+import java.util.ArrayList;
 import java.util.List;
 
 class Flattener {
 
+    @SuppressWarnings("unchecked")
     List<Object> flatten(List<Object> list) {
-	return list;
+	List<Object> flatList = new ArrayList<>();
+	for (Object o : list) {
+	    if (o instanceof List) {
+		flatList.addAll((List<Object>) o);
+	    } else {
+		flatList.add(o);
+	    }
+	}
+	return flatList;
     }
 }
