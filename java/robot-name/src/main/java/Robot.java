dca438efdf5827116bf901be68beb3ff1c8c9f29
Paweł J <jpawz@outlook.com>
2023-05-03 21:42:43 +0200

    Implement Robot class

--- java/robot-name/src/main/java/Robot.java
+++ java/robot-name/src/main/java/Robot.java
@@ -1,10 +1,9 @@
-/*
+class Robot {
+    String getName() {
+	return "RX837";
+    }
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
-
-Please remove this comment when submitting your solution.
-
-*/
+    public void reset() {
+	
+    }
+}
