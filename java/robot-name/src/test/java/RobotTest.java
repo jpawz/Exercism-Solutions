0c686f00f73b50a3d3f3ed534f2684c4457180f9
Paweł J <jpawz@outlook.com>
2023-05-07 21:00:15 +0200

    Code reformat

--- java/robot-name/src/test/java/RobotTest.java
+++ java/robot-name/src/test/java/RobotTest.java
@@ -8,49 +8,49 @@ import org.junit.Test;
 
 public class RobotTest {
 
-    private static final String EXPECTED_ROBOT_NAME_PATTERN = "[A-Z]{2}\\d{3}";
-    private Robot robot;
+	private static final String EXPECTED_ROBOT_NAME_PATTERN = "[A-Z]{2}\\d{3}";
+	private Robot robot;
 
-    @Before
-    public void setUp() {
-	robot = new Robot();
-    }
-
-    @Test
-    public void hasName() {
-	assertIsValidName(robot.getName());
-    }
-
-    @Test
-    public void sameRobotsHaveSameNames() {
-	assertThat(robot.getName()).isEqualTo(robot.getName());
-    }
-
-    @Test
-    public void differentRobotsHaveDifferentNames() {
-	assertThat(robot.getName()).isNotEqualTo(new Robot().getName());
-    }
-
-    @Test
-    public void resetName() {
-	final String name = robot.getName();
-	robot.reset();
-	final String name2 = robot.getName();
-	assertThat(name).isNotEqualTo(name2);
-	assertIsValidName(name2);
-    }
-
-    @Test
-    public void robotNamesAreUnique() {
-	Set<String> robotNames = new HashSet<>();
-	int sampleSize = 5000;
-	for (int i = 0; i < sampleSize; i++) {
-	    robotNames.add(new Robot().getName());
+	@Before
+	public void setUp() {
+		robot = new Robot();
 	}
-	assertThat(robotNames).hasSize(sampleSize);
-    }
 
-    private static void assertIsValidName(String name) {
-	assertThat(name).matches(EXPECTED_ROBOT_NAME_PATTERN);
-    }
+	@Test
+	public void hasName() {
+		assertIsValidName(robot.getName());
+	}
+
+	@Test
+	public void sameRobotsHaveSameNames() {
+		assertThat(robot.getName()).isEqualTo(robot.getName());
+	}
+
+	@Test
+	public void differentRobotsHaveDifferentNames() {
+		assertThat(robot.getName()).isNotEqualTo(new Robot().getName());
+	}
+
+	@Test
+	public void resetName() {
+		final String name = robot.getName();
+		robot.reset();
+		final String name2 = robot.getName();
+		assertThat(name).isNotEqualTo(name2);
+		assertIsValidName(name2);
+	}
+
+	@Test
+	public void robotNamesAreUnique() {
+		Set<String> robotNames = new HashSet<>();
+		int sampleSize = 5000;
+		for (int i = 0; i < sampleSize; i++) {
+			robotNames.add(new Robot().getName());
+		}
+		assertThat(robotNames).hasSize(sampleSize);
+	}
+
+	private static void assertIsValidName(String name) {
+		assertThat(name).matches(EXPECTED_ROBOT_NAME_PATTERN);
+	}
 }
