9e4177e44d4d4bad53a53308c24b9276293578c0
Paweł J <jpawz@outlook.com>
2023-05-07 18:57:16 +0200

    Code reformat

--- java/robot-name/src/main/java/Robot.java
+++ java/robot-name/src/main/java/Robot.java
@@ -4,30 +4,35 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 class Robot {
-    private static int counter = 0;
-    private final static List<String> nameLibrary;
+	private static int counter = 0;
+	private final static List<String> nameLibrary;
 
-    private String name;
+	private String name;
 
-    static {
-	nameLibrary = new ArrayList<>();
-	IntStream.range(0, 1000).boxed()
-		 .forEach(number -> IntStream.rangeClosed('A', 'Z').boxed()
-					     .forEach(firsChar -> IntStream.rangeClosed('A', 'Z').boxed().forEach(
-						 secondChar -> nameLibrary.add(
-						     String.format("%c%c%03d", firsChar, secondChar, number)))));
-	Collections.shuffle(nameLibrary);
-    }
+	static {
+		nameLibrary = new ArrayList<>();
+		IntStream.range(0, 1000)
+				.boxed()
+				.forEach(number -> IntStream.rangeClosed('A', 'Z')
+						.boxed()
+						.forEach(firsChar -> IntStream.rangeClosed('A', 'Z')
+								.boxed()
+								.forEach(
+										secondChar -> nameLibrary.add(
+												String.format("%c%c%03d", firsChar,
+														secondChar, number)))));
+		Collections.shuffle(nameLibrary);
+	}
 
-    Robot() {
-	name = nameLibrary.get(counter++);
-    }
+	Robot() {
+		name = nameLibrary.get(counter++);
+	}
 
-    String getName() {
-	return name;
-    }
+	String getName() {
+		return name;
+	}
 
-    void reset() {
-	name = nameLibrary.get(counter++);
-    }
+	void reset() {
+		name = nameLibrary.get(counter++);
+	}
 }
