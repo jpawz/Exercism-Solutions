0ed2f7b8360018be8ffe792a2f996820632cbb28
Paweł J <jpawz@outlook.com>
2023-05-10 21:24:02 +0200

    Fix compilation errors

--- java/binary-search/src/main/java/BinarySearch.java
+++ java/binary-search/src/main/java/BinarySearch.java
@@ -1,20 +1,29 @@
+import java.util.List;
 
 class BinarySearch {
 
-    int search(int a, int [] arr){
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	public BinarySearch(List<Integer> sortedList) {
+		// TODO Auto-generated constructor stub
+	}
 
-    int search(float a, float [] arr){
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	int search(int a, int[] arr) {
+		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+	}
 
-    int search(double a, double [] arr){
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	int search(float a, float[] arr) {
+		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+	}
 
-    int search(long a,long [] arr){
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	int search(double a, double[] arr) {
+		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+	}
+
+	int search(long a, long[] arr) {
+		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+	}
+
+	public int indexOf(int i) {
+		return 0;
+	}
 
 }
