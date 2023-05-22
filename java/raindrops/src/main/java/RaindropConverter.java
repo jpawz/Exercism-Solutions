f58d9cf51794c65a27fb9e5941b96b2269093bca
Paweł J <jpawz@outlook.com>
2023-05-22 22:40:29 +0200

    Enable all test cases

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -11,7 +11,7 @@ class RaindropConverter {
 		if (number % 7 == 0) {
 			sound.append("Plong");
 		}
-		if (number % 3 != 0 && number % 5 != 00 && number % 7 != 0) {
+		if (number % 3 != 0 && number % 5 != 0 && number % 7 != 0) {
 			sound.append(number);
 		}
 		return sound.toString();
