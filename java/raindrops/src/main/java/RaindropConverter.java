b12fee3476070fe1ccbc1457f833cfdaacd0d636
Paweł J <jpawz@outlook.com>
2023-05-22 22:35:38 +0200

    Pass first two test cases

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,20 @@
 class RaindropConverter {
 
-    String convert(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	String convert(int number) {
+		StringBuilder sound = new StringBuilder();
+		if (number % 3 == 0) {
+			sound.append("Pling");
+		}
+		if (number % 5 == 0) {
+			sound.append("Plang");
+		}
+		if (number % 7 == 0) {
+			sound.append("Plong");
+		}
+		if (number % 3 != 0 && number % 5 != 00 && number % 7 != 0) {
+			sound.append(number);
+		}
+		return sound.toString();
+	}
 
 }
