4e70ef62ae265d79c2b06299e0faf1a0dd26ecb9
Paweł J <jpawz@outlook.com>
2023-05-26 20:01:58 +0200

    Add PigLatinTranslator class

--- java/pig-latin/src/main/java/PigLatinTranslator.java
+++ java/pig-latin/src/main/java/PigLatinTranslator.java
@@ -1,10 +1,7 @@
-/*
+class PigLatinTranslator {
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+	public String translate(String word) {
+		return word + "ay";
+	}
 
-Please remove this comment when submitting your solution.
-
-*/
+}
