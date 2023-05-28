6366755f410ea98974bfc35b08121ed1ffd9f7fe
Paweł J <jpawz@outlook.com>
2023-05-28 20:52:17 +0200

    Change translate method to handle any vowel

--- java/pig-latin/src/main/java/PigLatinTranslator.java
+++ java/pig-latin/src/main/java/PigLatinTranslator.java
@@ -1,7 +1,15 @@
 class PigLatinTranslator {
 
+	private static final String VOWEL = "aeiouAEIOU";
+
 	public String translate(String word) {
-		return word + "ay";
+		if (isVowel(word.charAt(0)))
+			return word + "ay";
+		return word;
+	}
+
+	private static boolean isVowel(char c) {
+		return VOWEL.indexOf(c) != -1;
 	}
 
 }
