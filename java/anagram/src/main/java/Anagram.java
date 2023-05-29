312410fd717d9bd419370c851eef51461d6107fd
Paweł J <jpawz@outlook.com>
2023-05-29 19:39:09 +0200

    Add Anagram class

---
+++ java/anagram/src/main/java/Anagram.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Anagram {
+
+	public Anagram(String word) {
+	}
+
+	public List<String> match(List<String> wordList) {
+		return new ArrayList<>();
+	}
+
+}
