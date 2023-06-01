623e875a6752e729641452690f8444a8ec75a87b
Paweł J <jpawz@outlook.com>
2023-06-01 21:12:06 +0200

    Implement score method

---
+++ java/darts/src/main/java/Darts.java
@@ -0,0 +1,13 @@
+class Darts {
+	int score(double xOfDart, double yOfDart) {
+		double hit = Math.sqrt(xOfDart * xOfDart + yOfDart * yOfDart);
+		if (hit <= 10 && hit > 5)
+			return 1;
+		if (hit <= 5 && hit > 1)
+			return 5;
+		if (hit <= 0)
+			return 10;
+
+		return 0;
+	}
+}
