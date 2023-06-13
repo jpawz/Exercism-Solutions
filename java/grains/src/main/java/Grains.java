ccc1200fa6fe87a6e013dcc1a9dda0c27161f2cb
Paweł J <jpawz@outlook.com>
2023-06-13 22:40:54 +0200

    Implement grainsOnSquare method

--- java/grains/src/main/java/Grains.java
+++ java/grains/src/main/java/Grains.java
@@ -2,12 +2,16 @@ import java.math.BigInteger;
 
 class Grains {
 
-    BigInteger grainsOnSquare(final int square) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	BigInteger grainsOnSquare(final int square) {
+		BigInteger result = new BigInteger("1");
+		for (int i = 1; i < square; i++) {
+			result = result.add(result);
+		}
+		return result;
+	}
 
-    BigInteger grainsOnBoard() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	BigInteger grainsOnBoard() {
+		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+	}
 
 }
