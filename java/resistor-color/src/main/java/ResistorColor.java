ae37ec6ee4a32207ca1fe3f78525068fc3e62497
Paweł J <jpawz@outlook.com>
2023-06-16 22:13:38 +0200

    Implement colorCode method

--- java/resistor-color/src/main/java/ResistorColor.java
+++ java/resistor-color/src/main/java/ResistorColor.java
@@ -1,9 +1,14 @@
-class ResistorColor {
-    int colorCode(String color) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+import java.util.Map;
 
-    String[] colors() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+class ResistorColor {
+	private static final Map<String, Integer> RESISTORS = Map.of("black", 0, "brown", 1, "red", 2, "orange", 3,
+			"yellow", 4, "green", 5, "blue", 6, "violet", 7, "grey", 8, "white", 9);
+
+	int colorCode(String color) {
+		return RESISTORS.get(color);
+	}
+
+	String[] colors() {
+		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+	}
 }
