ebb8eb06591bc21eb5901339238ed60dfd049a42
Paweł J <jpawz@outlook.com>
2023-06-20 22:26:11 +0200

    Refactor to stream

--- java/micro-blog/src/main/java/MicroBlog.java
+++ java/micro-blog/src/main/java/MicroBlog.java
@@ -1,13 +1,15 @@
+import java.util.stream.IntStream;
+
 class MicroBlog {
 	public String truncate(String input) {
 		if (input.length() <= 5)
 			return input;
 
-		int lastIndex = input.length();
-		int codePointCount = 0;
-		do {
-			codePointCount = input.codePointCount(0, lastIndex--);
-		} while (codePointCount > 5);
-		return input.substring(0, lastIndex + 1);
+		int lastIndex = IntStream.range(0, input.length())
+				.filter(i -> input.codePointCount(0, i + 1) > 5)
+				.findFirst()
+				.orElse(input.length());
+
+		return input.substring(0, lastIndex);
 	}
 }
