ab924489b2bdd8cdd7bc3a0a5d4773d370a3a065
Paweł J <jpawz@outlook.com>
2023-06-20 22:55:13 +0200

    Simplification by applying offsetByCodePoints

--- java/micro-blog/src/main/java/MicroBlog.java
+++ java/micro-blog/src/main/java/MicroBlog.java
@@ -1,15 +1,8 @@
-import java.util.stream.IntStream;
-
 class MicroBlog {
 	public String truncate(String input) {
 		if (input.length() <= 5)
 			return input;
 
-		int lastIndex = IntStream.range(0, input.length())
-				.filter(i -> input.codePointCount(0, i + 1) > 5)
-				.findFirst()
-				.orElse(input.length());
-
-		return input.substring(0, lastIndex);
+		return input.substring(0, input.offsetByCodePoints(0, 5));
 	}
 }
