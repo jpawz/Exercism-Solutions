e1c13e0d8be6bd75ef7915e2acce4353c06ad50c
Paweł J <jpawz@outlook.com>
2023-06-27 22:43:19 +0200

    Solve for case 'A' and 'B'

--- java/diamond/src/main/java/DiamondPrinter.java
+++ java/diamond/src/main/java/DiamondPrinter.java
@@ -2,8 +2,20 @@ import java.util.List;
 
 class DiamondPrinter {
 
-    List<String> printToList(char a) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	List<String> printToList(char a) {
+		if (a == 'A') {
+			return List.of("A");
+		}
+
+		int lineLength = a - 'A' + 1;
+
+		int leadingSpaces = lineLength / 2;
+		String firstRow = String
+				.format("%" + (leadingSpaces + 1) + "s%-" + (leadingSpaces - 1 == 0 ? 1 : leadingSpaces - 1) + "s", 'A',
+						' ');
+		String middleRow = String.format("%-" + lineLength + "s%s", a, a);
+
+		return List.of(firstRow, middleRow, firstRow);
+	}
 
 }
