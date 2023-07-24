9afc13863a4e00f71ce3f085f39ea181b340b9f7
Paweł J <jpawz@outlook.com>
2023-07-24 23:21:16 +0200

    Refactor

--- java/proverb/src/main/java/Proverb.java
+++ java/proverb/src/main/java/Proverb.java
@@ -1,24 +1,21 @@
-import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class Proverb {
 
-	private final List<String> words;
+	private final String[] words;
 
 	Proverb(String[] words) {
-		this.words = Arrays.asList(words);
+		this.words = words;
 	}
 
 	String recite() {
-		if (words.size() == 0)
+		if (words.length == 0)
 			return "";
-		return IntStream.range(1, words.size())
-				.boxed()
-				.map(n -> String.format("For want of a %s the %s was lost.", words.get(n - 1), words.get(n)))
+		return IntStream.range(0, words.length - 1)
+				.mapToObj(n -> String.format("For want of a %s the %s was lost.", words[n], words[n + 1]))
 				.collect(Collectors.joining("\n"))
-				.concat(String.format("\nAnd all for the want of a %s.", words.get(0)))
+				.concat(String.format("\nAnd all for the want of a %s.", words[0]))
 				.strip();
 	}
 
