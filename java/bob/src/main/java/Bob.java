91e40d91d3c424b164ee6656c1e50d85ffba709d
Paweł J <jpawz@outlook.com>
2023-08-29 00:18:23 +0200

    Fix predicates

--- java/bob/src/main/java/Bob.java
+++ java/bob/src/main/java/Bob.java
@@ -1,3 +1,5 @@
+import static java.util.function.Predicate.not;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Predicate;
@@ -5,16 +7,16 @@ import java.util.function.Predicate;
 class Bob {
 
 	private static final Map<Predicate<String>, String> RESPONSES = new HashMap<>();
-	private static final Predicate<String> isQuestion = s -> s.endsWith("?");
-	private static final Predicate<String> isYell = s -> s.equals(s.toUpperCase());
+	private static final Predicate<String> isQuestion = s -> s.trim().endsWith("?");
+	private static final Predicate<String> isYell = s -> s.equals(s.toUpperCase()) && !s.equals(s.toLowerCase());
 	private static final Predicate<String> isYellQuestion = isYell.and(isQuestion);
 	private static final Predicate<String> isSilence = s -> s.isBlank();
 
 	private static final String defaultAnswer = "Whatever.";
 
 	static {
-		RESPONSES.put(isQuestion, "Sure.");
-		RESPONSES.put(isYell, "Whoa, chill out!");
+		RESPONSES.put(isQuestion.and(not(isYell)), "Sure.");
+		RESPONSES.put(isYell.and(not(isQuestion)), "Whoa, chill out!");
 		RESPONSES.put(isYellQuestion, "Calm down, I know what I'm doing!");
 		RESPONSES.put(isSilence, "Fine. Be that way!");
 	}
