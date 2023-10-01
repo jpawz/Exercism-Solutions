5f25ec5cfa800a5b21fe75834e5472f12e993169
Paweł J <jpawz@outlook.com>
2023-10-01 22:53:12 +0200

    Solve calculator-conundrum exercise

--- java/calculator-conundrum/src/main/java/CalculatorConundrum.java
+++ java/calculator-conundrum/src/main/java/CalculatorConundrum.java
@@ -1,5 +1,8 @@
 class CalculatorConundrum {
 	public String calculate(int operand1, int operand2, String operation) {
+		if (operation == null) {
+			throw new IllegalArgumentException("Operation cannot be null");
+		}
 		switch (operation) {
 		case "+":
 			return String.format("%d + %d = %d", operand1, operand2, operand1 + operand2);
@@ -8,9 +11,14 @@ class CalculatorConundrum {
 		case "*":
 			return String.format("%d * %d = %d", operand1, operand2, operand1 * operand2);
 		case "/":
+			if (operand2 == 0) {
+				throw new IllegalOperationException("Division by zero is not allowed", new ArithmeticException());
+			}
 			return String.format("%d / %d = %d", operand1, operand2, operand1 / operand2);
+		case "":
+			throw new IllegalArgumentException("Operation cannot be empty");
 		default:
-			throw new IllegalOperationException("");
+			throw new IllegalOperationException(String.format("Operation '%s' does not exist", operation));
 		}
 	}
 }
