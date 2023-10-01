8c6511b07d25c1a008d456fec0484742290c4c41
Paweł J <jpawz@outlook.com>
2023-10-01 23:17:27 +0200

    Refactoring

--- java/calculator-conundrum/src/main/java/CalculatorConundrum.java
+++ java/calculator-conundrum/src/main/java/CalculatorConundrum.java
@@ -1,24 +1,39 @@
 class CalculatorConundrum {
 	public String calculate(int operand1, int operand2, String operation) {
+		validateInputs(operand1, operand2, operation);
+
+		String operationFormat = "%d %s %d = %s";
+		String result = null;
+
+		switch (operation) {
+		case "+":
+			result = String.format(operationFormat, operand1, operation, operand2, operand1 + operand2);
+			break;
+		case "-":
+			result = String.format(operationFormat, operand1, operation, operand2, operand1 - operand2);
+			break;
+		case "*":
+			result = String.format(operationFormat, operand1, operation, operand2, operand1 * operand2);
+			break;
+		case "/":
+			result = String.format(operationFormat, operand1, operation, operand2, operand1 / operand2);
+		}
+
+		return result;
+	}
+
+	private void validateInputs(int operand1, int operand2, String operation) {
 		if (operation == null) {
 			throw new IllegalArgumentException("Operation cannot be null");
 		}
-		switch (operation) {
-		case "+":
-			return String.format("%d + %d = %d", operand1, operand2, operand1 + operand2);
-		case "-":
-			return String.format("%d - %d = %d", operand1, operand2, operand1 - operand2);
-		case "*":
-			return String.format("%d * %d = %d", operand1, operand2, operand1 * operand2);
-		case "/":
-			if (operand2 == 0) {
-				throw new IllegalOperationException("Division by zero is not allowed", new ArithmeticException());
-			}
-			return String.format("%d / %d = %d", operand1, operand2, operand1 / operand2);
-		case "":
-			throw new IllegalArgumentException("Operation cannot be empty");
-		default:
-			throw new IllegalOperationException(String.format("Operation '%s' does not exist", operation));
+		if ("/".equals(operation) && operand2 == 0) {
+			throw new IllegalOperationException("Division by zero is not allowed", new ArithmeticException());
 		}
+		if (operation.isBlank()) {
+			throw new IllegalArgumentException("Operation cannot be empty");
+		}
+		if (!"+-*/".contains(operation))
+			throw new IllegalOperationException(String.format("Operation '%s' does not exist", operation));
 	}
+
 }
